519f8455298cea15774711573aa31dcd7dd613b0
acooma <acoozee@gmail.com>
2019-10-05 13:28:16 -0500

    Updates

---
+++ precache-manifest.f3974df64e2c1dfa92871d24583bc9a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb218521cca9ae9372d88b0cfa28725",
+    "url": "/490-assign1/index.html"
+  },
+  {
+    "revision": "0a5495ab28e7ec2a769a",
+    "url": "/490-assign1/static/css/main.f49c067a.chunk.css"
+  },
+  {
+    "revision": "138f3fb6807bd3e20d58",
+    "url": "/490-assign1/static/js/2.30bad417.chunk.js"
+  },
+  {
+    "revision": "0a5495ab28e7ec2a769a",
+    "url": "/490-assign1/static/js/main.308d196f.chunk.js"
+  },
+  {
+    "revision": "3c22182a35617ecb89b6",
+    "url": "/490-assign1/static/js/runtime-main.26744149.js"
+  }
+]);
